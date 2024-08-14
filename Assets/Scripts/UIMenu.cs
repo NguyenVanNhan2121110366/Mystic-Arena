f6bc86d048adba5d019c98d12cffb2c7801e51b0
Nguyễn Nhân <144342490+NguyenVanNhan2121110366@users.noreply.github.com>
2024-08-14 23:08:42 +0700

    14/08/2024 11:08 PM

--- Assets/Scripts/UIMenu.cs
+++ Assets/Scripts/UIMenu.cs
@@ -4,13 +4,40 @@ using UnityEngine.UI;
 public class UIMenu : MonoBehaviour
 {
     [SerializeField] private Button bntStart;
+    [SerializeField] private Button bntShop;
+    [SerializeField] private GameObject chooseShop;
+    [SerializeField] private Button bntShopItem;
+    [SerializeField] private Button bntShopSkill;
+    private bool checkChooseShop;
 
     private void Awake()
     {
         this.bntStart.onClick.AddListener(ClickStart);
+        this.bntShop.onClick.AddListener(ClickShop);
+    }
+    private void Start()
+    {
+        this.checkChooseShop = false;
     }
     private void ClickStart()
     {
         SceneManager.LoadScene("Game");
+    }
+
+    private void ClickShop()
+    {
+        if (checkChooseShop)
+            this.chooseShop.SetActive(true);
+        else
+            this.chooseShop.SetActive(true);
+    }
+    private void ClickShopItem()
+    {
+
+    }
+
+    private void ClickShopSkill()
+    {
+
     }
 }
